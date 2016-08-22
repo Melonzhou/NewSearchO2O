10cd168441f5b8f3353f95507cbb741a6c7cefc1
zhouyuanyuan <zhouyuanyuan@baidu.com>
2016-08-22 17:35:16 +0800

    新建工程

---
+++ app/src/main/java/com/baidu/newsearch/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.baidu.newsearch;
+
+import android.app.Application;
+
+/**
+ * Created by Melon on 16/8/22.
+ */
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
---
+++ app/src/main/java/com/baidu/newsearch/MainTabActivity.java
@@ -0,0 +1,13 @@
+package com.baidu.newsearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_tab_activity_layout);
+    }
+}
